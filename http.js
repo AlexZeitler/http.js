c71fac210fe9203e27de46bb698e86f5243a9ed8
Golo Roden <webmaster@goloroden.de>
2012-08-26 12:05:57 +0200

    http.get now works.

--- http.js
+++ http.js
@@ -1,25 +1,25 @@
 (function (root) {
   var getXhr = function (callback) {
     if (window.XMLHttpRequest) {
-      callback(null, new XMLHttpRequest());
+      return callback(null, new XMLHttpRequest());
     } else if (window.ActiveXObject) {
       try {
-        callback(null, ActiveXObject("Msxml2.XMLHTTP"));
+        return callback(null, ActiveXObject("Msxml2.XMLHTTP"));
       } catch (e) {
-        callback(null, ActiveXObject("Microsoft.XMLHTTP"));
+        return callback(null, ActiveXObject("Microsoft.XMLHTTP"));
       }
     }
-    callback(new Error());
+    return callback(new Error());
   };
 
   var http = {
     get: function (url, callback) {
       getXhr(function (err, xhr) {
-        if(err) callback(err);
-        xhr.open('GET', url)
+        if(err) return callback(err);
+        xhr.open('GET', url);
         xhr.onreadystatechange = function () {
           if(xhr.readyState === 4) {
-            callback(null, xhr.status, xhr.responseText || '')
+            callback(xhr.status, xhr.responseText || '')
           }
         };
         xhr.send(null);
